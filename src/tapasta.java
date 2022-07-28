187cd7b3d543f0aef32e588baa1a33224ad492c0
user11 <pehotskijdanil@gmail.com>
2022-07-28 16:24:59 +0200

    sovenchoke

--- src/tapasta.java
+++ src/tapasta.java
@@ -1,3 +1,21 @@
 public class tapasta {
+    public static void main(String[] args) {
+        int calk;
+        calk = 10-9;
+        System.out.println(calk);
+        calk = 124+100;
+        System.out.println(calk);
+        calk =200*12;
+        System.out.println(calk);
+        calk =21/3;
+        System.out.println(calk);
+    }
 
-}
+
+
+    }
+
+
+
+
+
